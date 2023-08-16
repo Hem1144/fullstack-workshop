afe9429d0bfa0b5b324d77b7e6cca463b6b2de68
Hem1144 <hemlaldulal.986@gmail.com>
2023-08-16 16:49:15 +0545

    Extend session time for login

--- notes-server/controllers/login.js
+++ notes-server/controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post("/", async (request, response) => {
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 30,
+    expiresIn: 3000,
   });
 
   response
