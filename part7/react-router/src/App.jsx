83d6992a1efe611a7169370b23d8fb56d7930412
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-15 16:18:58 +0545

    feat: Add useNavigate to programmatically navigate to a url

--- part7/react-router/src/App.jsx
+++ part7/react-router/src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import Notes from "./Notes";
 import Note from "./Note";
+import { useState } from "react";
+import Login from "./Login";
 
 const notes = [
   {
@@ -32,6 +34,7 @@ const Users = () => (
   </div>
 );
 const App = () => {
+  const [user, setUser] = useState(null);
   const padding = {
     padding: 5,
   };
@@ -48,13 +51,23 @@ const App = () => {
         <Link style={padding} to="/users">
           users
         </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to="/login">
+            login
+          </Link>
+        )}
       </div>
 
       <Routes>
         <Route path="/notes/:id" element={<Note notes={notes} />} />
-
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route path="/users" element={<Users />} />
+        <Route
+          path="/users"
+          element={user ? <Users /> : <Navigate replace to="/login" />}
+        />{" "}
         <Route path="/" element={<Home />} />
       </Routes>
 
---
+++ part7/react-router/src/Login.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+const Login = ({ setUser }) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.username.value);
+    setUser(event.target.username.value);
+    navigate("/");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="username" />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Login;
