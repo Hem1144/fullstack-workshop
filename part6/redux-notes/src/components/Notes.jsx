7a22fb42dc42e20d3a811e6d10383fac2d1c1f0d
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-15 14:47:18 +0545

    feat: Many reduces completed of part6

--- part6/redux-notes/src/components/Notes.jsx
+++ part6/redux-notes/src/components/Notes.jsx
@@ -3,7 +3,29 @@ import { toggleImportantOf } from "../reducers/noteReducer";
 
 const Notes = () => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
+
+  const filter = useSelector((state) => state.filter);
+
+  const notes = useSelector((state) => {
+    if (filter === "ALL") {
+      return state.notes;
+    }
+    if (filter === "IMPORTANT") {
+      return state.notes.filter((note) => {
+        if (note.important === true) {
+          return true;
+        }
+      });
+    }
+    if (filter === "NONIMPORTANT") {
+      return state.notes.filter((note) => {
+        if (note.important === false) {
+          return true;
+        }
+      });
+    }
+    return state.notes;
+  });
 
   const toggleImportant = (id) => {
     dispatch(toggleImportantOf(id));
