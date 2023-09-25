00a068b2d7a9a1f90ba7cffb0318aebaa873c269
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-25 14:45:30 +0545

    Completed, Additional notes on relational database

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -42,6 +42,8 @@ Note.init(
   }
 );
 
+Note.sync(); //! It create table accoding to your given schema
+
 app.get("/api/notes", async (req, res) => {
   // const notes = await sequelize.query("SELECT * FROM notes", {  //! Using SQL command
   //   type: QueryTypes.SELECT,
@@ -51,10 +53,34 @@ app.get("/api/notes", async (req, res) => {
   res.json(notes);
 });
 
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  // console.log(note.toJSON());
+  console.log(JSON.stringify(note, null, 2));
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).send("no data found");
+  }
+});
+
 app.post("/api/notes", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const note = await Note.create(req.body);
   res.json(note);
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  console.log(note.toJSON());
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = process.env.PORT || 3001;
