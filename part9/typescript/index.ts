f81aad7bc4ee75f76dc7f51c8d3808e59a9bd264
Hem1144 <hemlaldulal.986@gmail.com>
2023-10-04 15:05:28 +0545

    feat: Completed, Add api endpoint for calculate

--- part9/typescript/index.ts
+++ part9/typescript/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
-
 const app = express();
+
+import { calculator, Operation } from "./multiplier";
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  const { value1, value2, op } = req.body;
+
+  const operation = op as Operation;
+
+  const result = calculator(Number(value1), Number(value2), operation);
+  res.send({ result });
 });
 
 const PORT = 3003;
