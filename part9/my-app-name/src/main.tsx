79dd690ea7c94be2e879e4b0a09b3061cbe91db1
Hem1144 <hemlaldulal.986@gmail.com>
2023-10-06 14:33:52 +0545

    feat: Completed, Deeper type usage in react app

--- part9/my-app-name/src/main.tsx
+++ part9/my-app-name/src/main.tsx
@@ -1,13 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps): JSX.Element => {
-  return <h1>Hello, {props.name}</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Welcome name="Sarah" />
-) 
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
